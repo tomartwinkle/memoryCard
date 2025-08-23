9e4c9dd4d15c275bdfc028ac0eb4da15aca027d8
Twinkle <tomartwinkle2572gmail.com>
2025-08-24 02:06:10 +0530

    Redo

--- src/components/scoreboard.jsx
+++
@@ -1,9 +0,0 @@
-// Scoreboard.jsx
-export default function Scoreboard({ currentScore, highestScore }) {
-  return (
-    <div style={{ marginBottom: "20px" }}>
-      <h2>Score: {currentScore}</h2>
-      <h3>Highest Score: {highestScore}</h3>
-    </div>
-  );
-}
